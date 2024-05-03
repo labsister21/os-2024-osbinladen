d38523e66333a7290e0e66e3ee45d48046b0dd00
RayNoor0 <rayendraalthaf04@gmail.com>
2024-05-03 20:07:51 +0700

    string concat

--- src/stdlib/string.c
+++ src/stdlib/string.c
@@ -51,3 +51,17 @@ uint8_t strlen(char* str){
     }
     return i;
 }
+
+void strcat(char* str1, char* str2){
+    int i = 0;
+    int j = 0;
+    while(str1[i] != '\0'){
+        i++;
+    }
+    while(str2[j] != '\0'){
+        str1[i] = str2[j];
+        i++;
+        j++;
+    }
+    str1[i] = '\0';
+}
