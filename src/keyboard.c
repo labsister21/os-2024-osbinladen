9d5e2c272948dee82931b7a0178ee6145a210a64
RayhanFadhlan <rayhanfazka07@gmail.com>
2024-05-09 21:52:48 +0700

    shift pressing on keyboard

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,3 +1,61 @@
+// #include "header/driver/keyboard.h"
+// #include "header/cpu/portio.h"
+// #include "header/stdlib/string.h"
+
+// static struct KeyboardDriverState keyboard_state = {
+//     .read_extended_mode = false,
+//     .keyboard_input_on = false,
+//     .keyboard_buffer = 0x0
+// };
+
+// const char keyboard_scancode_1_to_ascii_map[256] = {
+//       0, 0x1B, '1', '2', '3', '4', '5', '6',  '7', '8', '9',  '0',  '-', '=', '\b', '\t',
+//     'q',  'w', 'e', 'r', 't', 'y', 'u', 'i',  'o', 'p', '[',  ']', '\n',   0,  'a',  's',
+//     'd',  'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   0, '\\',  'z', 'x',  'c',  'v',
+//     'b',  'n', 'm', ',', '.', '/',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0, '-',    0,    0,   0,  '+',    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+// };
+
+// // Activate keyboard ISR / start listen keyboard & save to buffer
+// void keyboard_state_activate(void){
+//   activate_keyboard_interrupt();
+//   keyboard_state.keyboard_input_on = true;
+// }
+
+// void keyboard_state_deactivate(void){
+//   keyboard_state.keyboard_input_on = false;
+// }
+
+// void get_keyboard_buffer(char *buf){
+//   *buf = keyboard_state.keyboard_buffer;
+//   keyboard_state.keyboard_buffer = 0x0;
+// }
+
+// void keyboard_isr(void) {
+//     uint8_t scancode = in(KEYBOARD_DATA_PORT);
+
+//     if (keyboard_state.keyboard_input_on){
+//       char key = keyboard_scancode_1_to_ascii_map[scancode];
+//       keyboard_state.keyboard_buffer = key;
+//     }
+
+//     pic_ack(PIC1_OFFSET + IRQ_KEYBOARD);
+//     // TODO : Implement scancode processing
+// }
+
+
 #include "header/driver/keyboard.h"
 #include "header/cpu/portio.h"
 #include "header/stdlib/string.h"
@@ -5,7 +63,8 @@
 static struct KeyboardDriverState keyboard_state = {
     .read_extended_mode = false,
     .keyboard_input_on = false,
-    .keyboard_buffer = 0x0
+    .keyboard_buffer = 0x0,
+    .shift_pressed = false // Add a field to keep track of shift key state
 };
 
 const char keyboard_scancode_1_to_ascii_map[256] = {
@@ -28,6 +87,18 @@ const char keyboard_scancode_1_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
 
+const char keyboard_scancode_1_to_ascii_map_with_shift[256] = {
+    0, '~', '!', '@', '#', '$', '%', '^',  '&', '*', '(', ')', '_', '+', '\b', '\t',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n', 0, 'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', 0, '|', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', '<', '>', '?', 0, '*', 0, ' ', 0, 0, 0, 0, 0, 0,
+    0,    0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+', 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
 // Activate keyboard ISR / start listen keyboard & save to buffer
 void keyboard_state_activate(void){
   activate_keyboard_interrupt();
@@ -46,11 +117,30 @@ void get_keyboard_buffer(char *buf){
 void keyboard_isr(void) {
     uint8_t scancode = in(KEYBOARD_DATA_PORT);
 
-    if (keyboard_state.keyboard_input_on){
-      char key = keyboard_scancode_1_to_ascii_map[scancode];
-      keyboard_state.keyboard_buffer = key;
+    if (keyboard_state.keyboard_input_on) {
+        char key;
+
+        // Check if the shift key is pressed and modify the key accordingly
+        if ((scancode & 0x80) == 0x80) { // Check if key is released
+            if (scancode == 0xAA || scancode == 0xB6) { // Left or right shift key released
+                keyboard_state.shift_pressed = false;
+            }
+        } else {
+            if (scancode == 0x2A || scancode == 0x36) { // Left or right shift key pressed
+                keyboard_state.shift_pressed = true;
+            } else {
+                // Retrieve character from the appropriate map based on shift key state
+                if (keyboard_state.shift_pressed) {
+                    key = keyboard_scancode_1_to_ascii_map_with_shift[scancode];
+                } else {
+                    key = keyboard_scancode_1_to_ascii_map[scancode];
+                }
+                
+                keyboard_state.keyboard_buffer = key;
+            }
+        }
     }
 
     pic_ack(PIC1_OFFSET + IRQ_KEYBOARD);
-    // TODO : Implement scancode processing
+    // TODO: Implement scancode processing
 }
