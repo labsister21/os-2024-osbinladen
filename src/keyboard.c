2f7df6ebe37db779ab34144adaf85b32a3944b65
RayhanFadhlan <rayhanfazka07@gmail.com>
2024-05-12 18:12:31 +0700

    keyboard left arrow and right arrow not finished

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,61 +1,3 @@
-// #include "header/driver/keyboard.h"
-// #include "header/cpu/portio.h"
-// #include "header/stdlib/string.h"
-
-// static struct KeyboardDriverState keyboard_state = {
-//     .read_extended_mode = false,
-//     .keyboard_input_on = false,
-//     .keyboard_buffer = 0x0
-// };
-
-// const char keyboard_scancode_1_to_ascii_map[256] = {
-//       0, 0x1B, '1', '2', '3', '4', '5', '6',  '7', '8', '9',  '0',  '-', '=', '\b', '\t',
-//     'q',  'w', 'e', 'r', 't', 'y', 'u', 'i',  'o', 'p', '[',  ']', '\n',   0,  'a',  's',
-//     'd',  'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',   0, '\\',  'z', 'x',  'c',  'v',
-//     'b',  'n', 'm', ',', '.', '/',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0, '-',    0,    0,   0,  '+',    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-//       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
-// };
-
-// // Activate keyboard ISR / start listen keyboard & save to buffer
-// void keyboard_state_activate(void){
-//   activate_keyboard_interrupt();
-//   keyboard_state.keyboard_input_on = true;
-// }
-
-// void keyboard_state_deactivate(void){
-//   keyboard_state.keyboard_input_on = false;
-// }
-
-// void get_keyboard_buffer(char *buf){
-//   *buf = keyboard_state.keyboard_buffer;
-//   keyboard_state.keyboard_buffer = 0x0;
-// }
-
-// void keyboard_isr(void) {
-//     uint8_t scancode = in(KEYBOARD_DATA_PORT);
-
-//     if (keyboard_state.keyboard_input_on){
-//       char key = keyboard_scancode_1_to_ascii_map[scancode];
-//       keyboard_state.keyboard_buffer = key;
-//     }
-
-//     pic_ack(PIC1_OFFSET + IRQ_KEYBOARD);
-//     // TODO : Implement scancode processing
-// }
-
-
 #include "header/driver/keyboard.h"
 #include "header/cpu/portio.h"
 #include "header/stdlib/string.h"
@@ -64,7 +6,7 @@ static struct KeyboardDriverState keyboard_state = {
     .read_extended_mode = false,
     .keyboard_input_on = false,
     .keyboard_buffer = 0x0,
-    .shift_pressed = false // Add a field to keep track of shift key state
+    .shift_pressed = false 
 };
 
 const char keyboard_scancode_1_to_ascii_map[256] = {
@@ -120,7 +62,6 @@ void keyboard_isr(void) {
     if (keyboard_state.keyboard_input_on) {
         char key;
 
-        
         if ((scancode & 0x80) == 0x80) { 
             if (scancode == 0xAA || scancode == 0xB6) { 
                 keyboard_state.shift_pressed = false;
@@ -129,18 +70,22 @@ void keyboard_isr(void) {
             if (scancode == 0x2A || scancode == 0x36) { 
                 keyboard_state.shift_pressed = true;
             } else {
-               
-                if (keyboard_state.shift_pressed) {
-                    key = keyboard_scancode_1_to_ascii_map_with_shift[scancode];
+                if (scancode == 0x4B) {
+                    handle_left_arrow();
+                } else if (scancode == 0x4D) {
+                    handle_right_arrow();
                 } else {
-                    key = keyboard_scancode_1_to_ascii_map[scancode];
+                    if (keyboard_state.shift_pressed) {
+                        key = keyboard_scancode_1_to_ascii_map_with_shift[scancode];
+                    } else {
+                        key = keyboard_scancode_1_to_ascii_map[scancode];
+                    }
+                    
+                    keyboard_state.keyboard_buffer = key;
                 }
-                
-                keyboard_state.keyboard_buffer = key;
             }
         }
     }
 
     pic_ack(PIC1_OFFSET + IRQ_KEYBOARD);
-    // TODO: Implement scancode processing
 }
